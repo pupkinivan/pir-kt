376580ecbea7a92c45735bc473f91e42ea823b6b
Ivan Pupkin <ivan.pupkin@ank.app>
2022-06-18 17:29:34 -0300

    Adjust - replace CommandLineParser for Scanner

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,23 @@
-import org.apache.commons.cli.DefaultParser
-import org.apache.commons.cli.Option
-import org.apache.commons.cli.Options
 import pir.PirFileConverter
+import java.util.Scanner
 
-fun main(args: Array<String>) {
-    val cliOptions = Options()
-        .addRequiredOption("d", "directory", true, "Directory from which to read the PIR files (-d, --directory)")
-        .addOption(Option("t", "txt", false, "Export to .txt instead of CSV (-t, --txt)"))
-
-    val parser = DefaultParser()
-    val commandLine = parser.parse(cliOptions, args)
-
-    println("Program arguments: ${args.joinToString()}")
-    /*if (args.isEmpty()) {
-        println("Not enough arguments, at least one is expected for the directory of the PIR files")
-        exitProcess(1)
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("Enter the directory that contains the PIR files:")
+    val inputDirectory = scanner.nextLine()
+    println("Save as .txt (enter 'y') instead of CSV (default, press 'n' or enter)?")
+    val isSaveAsCsv: Boolean
+    while (true) {
+        val asCsvInput = scanner.nextLine()
+        if (asCsvInput !in setOf("", "y", "n"))
+            println("Wrong input. Enter 'n' for CSV, 'y' for .txt")
+        else {
+            isSaveAsCsv = asCsvInput != "y"
+            break
+        }
     }
-    else if (args.size > 2) {
-        println("Too many arguments")
-        exitProcess(2)
-    }*/
 
-    //val directory = args[0]
-    val directory = commandLine.getOptionValue("directory")
-    //val isSaveAsCsv = if (args.size == 2) args[1] == "--csv" else false
-    val isSaveAsCsv = !(commandLine.hasOption("txt"))
+    println("Program arguments:\n  - Directory: $inputDirectory\n  - Save as .txt?: $isSaveAsCsv")
 
-    PirFileConverter.runConversion(directory, isSaveAsCsv)
+    PirFileConverter.runConversion(inputDirectory, isSaveAsCsv)
 }
